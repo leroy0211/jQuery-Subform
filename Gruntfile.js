3cc96714e54d2ea14a72332b144b13249f07018b
Leroy <leroybaeyens@gmail.com>
2016-06-01 23:47:22 +0200

    installed Qunit with a dymmy test

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,11 +3,16 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+    qunit: {
+        all: ['test/*.html']
+    }
   });
+
+  grunt.loadNpmTasks('grunt-contrib-qunit');
 
 
   // Default task(s).
   grunt.registerTask('default', []);
 
-  grunt.registerTask('test', []);
+  grunt.registerTask('test', ['qunit']);
 };
